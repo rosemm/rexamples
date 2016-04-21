59a349e1baeedca8860c0b849edb89d6a489164e
Rose Maier <rosemhartman@gmail.com>
2016-04-21 15:52:17 -0700

    LCA script init

---
+++ LCA.R
@@ -0,0 +1,9 @@
+
+
+# latent class analysis
+# https://cran.r-project.org/web/packages/poLCA/README.html
+# https://cran.r-project.org/web/packages/poLCA/inst/doc/poLCA-manual-1-4.pdf?raw=true
+# https://cran.r-project.org/web/packages/profileR/vignettes/jss_profileR.pdf
+install.packages("poLCA", "profileR") # again, only need to run this one time
+library(poLCA)
+library(profileR)
