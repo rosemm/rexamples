cc6f01819a556537955441cd37525e0e35f130e0
Rose Maier <rosemhartman@gmail.com>
2016-04-21 19:39:24 -0700

    added variable for N, and Subj number

--- generating_data.R
+++ generating_data.R
@@ -1,12 +1,13 @@
 # making some toy data sets for examples
 
+N <- 20 # sample size
+
 # with Lickert scale
-resp1 <- sample(c("Strongly disagree", "Disagree", "Neither", "Agree", "Strongly Agree"), 20, replace=T)
-resp2 <- sample(c("Strongly disagree", "Disagree", "Neither", "Agree", "Strongly Agree"), 20, replace=T)
-resp3 <- sample(c("Strongly disagree", "Disagree", "Neither", "Agree", "Strongly Agree"), 20, replace=T)
-resp4 <- sample(c("Strongly disagree", "Disagree", "Neither", "Agree", "Strongly Agree"), 20, replace=T)
-resp5 <- sample(c("Strongly disagree", "Disagree", "Neither", "Agree", "Strongly Agree"), 20, replace=T)
+resp1 <- sample(c("Strongly disagree", "Disagree", "Neither", "Agree", "Strongly Agree"), N, replace=T)
+resp2 <- sample(c("Strongly disagree", "Disagree", "Neither", "Agree", "Strongly Agree"), N, replace=T)
+resp3 <- sample(c("Strongly disagree", "Disagree", "Neither", "Agree", "Strongly Agree"), N, replace=T)
+resp4 <- sample(c("Strongly disagree", "Disagree", "Neither", "Agree", "Strongly Agree"), N, replace=T)
+resp5 <- sample(c("Strongly disagree", "Disagree", "Neither", "Agree", "Strongly Agree"), N, replace=T)
 
-Lickert.data <- data.frame(resp1, resp2, resp3, resp4, resp5)
+Lickert.data <- data.frame(resp1, resp2, resp3, resp4, resp5, Subj=1:N)
 
-# with messy factor levels
