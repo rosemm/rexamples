1b6c671ef61525abc966c8432be0ecbcf05d6cf5
Rose Maier <rosemhartman@gmail.com>
2016-12-11 23:42:30 -0500

    added R-sq to the output; updated notation to match use in Rencher; added reference to Rencher

--- pkg/R/test_cc.R
+++ pkg/R/test_cc.R
@@ -4,7 +4,7 @@
 #' 
 #' @param cc the canonical correlations to test, output from from \code{\link[CCA]{cc}}
 #' 
-#' @return A data frame where each row is one dimension from the cannonical correlation (in order from largest to smallest), with columns for Wilk's Lambda, F statistic, df for the F test, and p value for the F test.
+#' @return A data frame where each row is one dimension from the cannonical correlation (in order from largest to smallest), with columns for R-squared, Wilk's Lambda, F statistic, df for the F test, and p value for the F test.
 #'
 #' @examples
 #' cc1 <- CCA::cc(iris[ , 1:2], iris[ , 3:4])
@@ -13,34 +13,36 @@
 #' @seealso \code{\link[CCA]{cc}}
 #' 
 #' @references \url{http://www.ats.ucla.edu/stat/r/dae/canonical.htm}
+#' @references Rencher, A. C. (2002). Methods of multivariate analysis, 2nd ed. John Wiley & Sons. p.367-p.370
 #'
 #' @export
 test_cc <- function(cc){
-  ev <- (1 - cc$cor^2)
+  r_sq <- cc$cor^2 # squared correlations
+  eigen_vals <- (1 - cc$cor^2)
   
   n <- length(cc$names$ind.names)
   p <- length(cc$names$Xnames)
   q <- length(cc$names$Ynames)
   k <- min(p, q)
-  m <- n - 3/2 - (p + q)/2
+  w <- n - 3/2 - (p + q)/2
   
-  w <- rev(cumprod(rev(ev)))
+  w_lambda <- rev(cumprod(rev(eigen_vals)))
   
   # initialize
   d1 <- d2 <- f <- vector("numeric", k)
   
   for (i in 1:k) {
-    s <- sqrt((p^2 * q^2 - 4)/(p^2 + q^2 - 5))
-    si <- 1/s
+    t <- sqrt((p^2 * q^2 - 4)/(p^2 + q^2 - 5))
+    ti <- 1/t
     d1[i] <- p * q
-    d2[i] <- m * s - p * q/2 + 1
-    r <- (1 - w[i]^si)/w[i]^si
+    d2[i] <- w * t - p * q/2 + 1
+    r <- (1 - w_lambda[i]^ti)/w_lambda[i]^ti
     f[i] <- r * d2[i]/d1[i]
-    p <- p - 1
-    q <- q - 1
+    p <- p - 1 # test remaining dimensions
+    q <- q - 1 # test remaining dimensions
   }
   
   pv <- pf(f, d1, d2, lower.tail = FALSE)
-  dmat <- data.frame(WilksL = w, F = f, df1 = d1, df2 = d2, p = pv)
+  dmat <- data.frame(R_sq=r_sq, WilksL = w_lambda, approx_F = f, df1 = d1, df2 = d2, p = pv)
   return(dmat)
 }
