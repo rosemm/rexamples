5a3a331aac6438f9e3ca645240e9ac0b5e25d758
Rose Maier <rosemhartman@gmail.com>
2017-04-10 16:07:34 -0400

    added improved logic for keep_going; added code to include articles with no abstract in the use_bib dataframe, for futher analysis

--- pkg/R/code_bibs.R
+++ pkg/R/code_bibs.R
@@ -1,6 +1,12 @@
 # library(RefManageR)
 # bib <- ReadBib("/Users/TARDIS/Documents/STUDIES/contexts_in_IDS/reports/savedrecs_all.bib", check = "error")
 
+# Results: 1,208
+# (from Web of Science Core Collection)
+# You searched for: TOPIC: (context) AND TOPIC: (infants)
+# Refined by: WEB OF SCIENCE CATEGORIES: ( PSYCHOLOGY DEVELOPMENTAL ) AND DOCUMENT TYPES: ( ARTICLE )
+# Timespan: All years. Indexes: SCI-EXPANDED, SSCI, ESCI.
+
 # code for suitability for meta-analysis
 code_bibs <- function(bib, dir = getwd(), use_bib_file="ma_bib.csv", return=TRUE, save=!return){
   stopifnot(require(RefManageR))
@@ -47,13 +53,30 @@ code_bibs <- function(bib, dir = getwd(), use_bib_file="ma_bib.csv", return=TRUE
       } 
       
       if(grepl(pattern="q", x=tolower(ma_use))){
-        all_done <- grepl(pattern="n", x=readline("Keep going? "))
+        keep_going <- readline("Keep going? ")
+        all_done <- grepl(pattern="n", x=tolower(keep_going))
+        if(grepl(pattern="y", x=tolower(keep_going))) {
+          # if user decides to keep going, step back one to re-evaluate the previous article
+          b <- b - 1
+        } 
       } else {
         # keep track of which records have already been coded
         coded <- c(coded, b)
       }
       
-    } 
+    } else {
+      # if abstract is not available for review, add the article to the use_bib dataframe for futher evaluation
+      title <- gsub(x=bib[b]$title, pattern = "[{](.*)[]}]", replacement = "\\1") # remove brackets
+      title <- gsub(x=title, pattern = "([[:space:]]+)", replacement = " ") # remove extra white space
+      
+      this_bib <- data.frame(author=paste(bib[b]$author, collapse = "; "), 
+                             year=as.numeric(gsub(x=bib[b]$year, pattern = ".*([[:digit:]]{4}).*", replacement = "\\1")),
+                             title=title, 
+                             journal=gsub(x=bib[b]$journal, pattern = "[{](.*)[]}]", replacement = "\\1"), 
+                             use="m", 
+                             Notes="Abstract not available.")
+      use_bib <- rbind(use_bib, this_bib)
+    }
     if(all_done==TRUE) {
       if(save){
         write.csv(use_bib, file=file.path(dir, use_bib_file), row.names = FALSE)
